b20d9da5344abbb57a17ad301b4db3d79d2fa24d
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-12-13 11:19:47 +0900

    やっと結果が出るようになった

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+#![allow(nonstandard_style)]
+
 use std::ffi::CStr;
 
 use libperl_sys::*;
 use libperl_sys::perl_core::PerlInterpreter;
-
 
 #[unsafe(no_mangle)]
 pub extern "C" fn boot_Mytest(my_perl_ptr: *mut PerlInterpreter, _cv: *mut CV) -> () {
@@ -12,10 +13,10 @@ pub extern "C" fn boot_Mytest(my_perl_ptr: *mut PerlInterpreter, _cv: *mut CV) -
     }
 
     static NAME1: &CStr = unsafe {
-        CStr::from_bytes_with_nul_unchecked(b"Mytest::mySum\0")
+        CStr::from_bytes_with_nul_unchecked(b"Mytest::is_even\0")
     };
     unsafe {
-        Perl_newXS_deffile(my_perl_ptr, NAME1.as_ptr(), Some(mySum_C))
+        Perl_newXS_deffile(my_perl_ptr, NAME1.as_ptr(), Some(is_even_C))
     };
 
     unsafe {
@@ -25,17 +26,17 @@ pub extern "C" fn boot_Mytest(my_perl_ptr: *mut PerlInterpreter, _cv: *mut CV) -
 
 
 #[unsafe(no_mangle)]
-pub extern "C" fn mySum_C(my_perl_ptr: *mut PerlInterpreter, cv: *mut CV) -> () {
+pub extern "C" fn is_even_C(my_perl_ptr: *mut PerlInterpreter, cv: *mut CV) -> () {
     if let Some(my_perl) = unsafe {my_perl_ptr.as_mut()} {
-        mySum(my_perl, cv)
+        is_even(my_perl, cv)
     } else {
         
     }
 }
 
-fn mySum(my_perl: &mut PerlInterpreter, cv: *mut CV) -> () {
+fn is_even(my_perl: &mut PerlInterpreter, cv: *mut CV) -> () {
     // dSP
-    let mut sp = my_perl.Istack_sp;
+    let sp = my_perl.Istack_sp;
 
     // dAXMARK
     let mut ax: Stack_off_t = unsafe {*my_perl.Imarkstack_ptr};
@@ -52,9 +53,43 @@ fn mySum(my_perl: &mut PerlInterpreter, cv: *mut CV) -> () {
         unsafe {Perl_croak_xs_usage(cv, msg.as_bytes().as_ptr() as *const i8)};
     }
 
-
     // int     input = (int)SvIV(ST(0))
+    let src = unsafe {*my_perl.Istack_base.add((ax + 0) as usize)};
+    let input = SvIV(my_perl, src);
 
+    let RETVAL = (input % 2) == 0;
+    println!("input {} RETVAL {}", input, RETVAL);
+
+    let targ = unsafe {Perl_sv_newmortal(my_perl)};
+    
+    // PUSHi((IV)RETVAL);
+    unsafe {
+        Perl_sv_setiv(my_perl, targ, RETVAL as i64)
+    };
+
+    unsafe {*sp = targ};
+
+    let off = 1;
+    my_perl.Istack_sp = unsafe {my_perl.Istack_base.add((ax + (off - 1)).try_into().unwrap())};
+
+    return ()
+
+}
+
+pub fn SvFLAGS(sv: *const libperl_sys::sv) -> u32 {
+    assert_ne!(sv, std::ptr::null_mut());
+    unsafe {(*sv).sv_flags}
+}
+
+fn SvIV(my_perl: &mut PerlInterpreter, sv: *mut libperl_sys::sv) -> i64 {
+    if (SvFLAGS(sv) & (SVf_IOK|SVs_GMG)) == SVf_IOK {
+        let xpviv = (unsafe {(*sv).sv_any}) as *const libperl_sys::xpviv;
+        return (unsafe {(*xpviv).xiv_u.xivu_iv}) as i64
+    }
+
+    let perl: *mut PerlInterpreter = my_perl;
+
+    return unsafe {Perl_sv_2iv(perl, sv)}
 }
 
 // #[cfg(test)]
